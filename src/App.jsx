046bf9ca5b3b457f851a19a49ef9b7835f1b8bfd
EsneiderCdD <ecd.97@hotmail.com>
2025-07-29 08:26:38 -0500

    feat: Zustand, rutas privadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import PerroDetalle from './pages/PerroDetalle'
 import Tienda from './pages/Tienda'
 import Test from './pages/Test'
 import LoginPage from './pages/LoginPage'
-
+import AdminPage from './pages/AdminPage'
+import useAuthStore from '@store/authStore' 
 
 function App() {
+  const isAuth = useAuthStore((state) => state.isAuthenticated())
+
   return (
     <Router>
       <Routes>
@@ -14,9 +18,14 @@ function App() {
         <Route path="/perros/:id" element={<PerroDetalle />} />
         <Route path="/tienda" element={<Tienda />} />
         <Route path="/test" element={<Test />} />
-
         <Route path="/login" element={<LoginPage />} />
 
+        <Route
+          path="/admin"
+          element={
+            isAuth ? <AdminPage /> : <Navigate to="/login" replace />
+          }
+        />
       </Routes>
     </Router>
   )
---
+++ src/pages/AdminPage.jsx
@@ -0,0 +1,13 @@
+// src/pages/AdminPage.jsx
+import styles from './AdminPage.module.css'
+
+const AdminPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Hola, Admin 👋</h1>
+      <p>Bienvenido al panel administrativo.</p>
+    </div>
+  )
+}
+
+export default AdminPage
