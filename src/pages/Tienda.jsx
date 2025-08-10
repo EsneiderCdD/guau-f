109848dc4db32d744d1a6e3aa0df760de0cfef1d
EsneiderCdD <ecd.97@hotmail.com>
2025-08-10 12:27:19 -0500

    feat: estilos Tienda.module.css

--- src/pages/Tienda.jsx
+++ src/pages/Tienda.jsx
@@ -10,6 +10,10 @@ const Tienda = () => {
   return (
     <>
       <Navbar />
+      <header className={styles.header}>
+        <img src="/Header2.png" alt="Header background" className={styles.background} />
+        <img src="/Gua-logo.png" alt="Gua Logo" className={styles.logo} />
+      </header>
       <div className={styles.wrapper}>
         {isLoading && <p>Cargando productos...</p>}
         {error && <p>Error: {error}</p>}
