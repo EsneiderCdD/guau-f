e7352b1a775ff81c44436839dd614cc8b96f5831
EsneiderCdD <ecd.97@hotmail.com>
2025-08-10 12:45:58 -0500

    feat : persistencia del estado con Zustand

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,14 +1,23 @@
 // src/stores/authStore.js
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
-const useAuthStore = create((set) => ({
-  user: null,
-  token: null,
+const useAuthStore = create(
+  persist(
+    (set, get) => ({
+      user: null,
+      token: null,
 
-  login: (user, token) => set({ user, token }),
-  logout: () => set({ user: null, token: null }),
+      login: (user, token) => set({ user, token }),
+      logout: () => set({ user: null, token: null }),
 
-  isAuthenticated: () => !!useAuthStore.getState().token
-}))
+      isAuthenticated: () => !!get().token
+    }),
+    {
+      name: 'auth-storage', // Nombre de la clave en localStorage
+      getStorage: () => localStorage // Puedes cambiarlo por sessionStorage si quieres que se borre al cerrar pestaña
+    }
+  )
+)
 
 export default useAuthStore
