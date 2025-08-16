cfee7a890bd0b750ed64c3bfea75bab2ea28f710
EsneiderCdD <ecd.97@hotmail.com>
2025-08-15 21:37:09 -0500

    feat: Nabvar Componente reutilziable

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,50 +1,26 @@
-// src/components/Navbar.jsx
-
+// src/components/Navbar/Navbar.jsx
 import { Link } from 'react-router-dom'
-import useAuthStore from '@store/authStore'
 import styles from './Navbar.module.css'
-import LoginButton from '../ui/LoginButton'
-import LogoutButton from '../ui/LogoutButton'
 
 const Navbar = () => {
-  const { user } = useAuthStore()
-
-  const handleClick = () => {
-    if (user?.rol === 'user') {
-      alert('Eres un usuario. ¡Bienvenido!')
-    }
-  }
-
   return (
-    <header className={styles.navbar}>
-      <nav className={styles.nav}>
-        <Link to="/" className={styles.link}>Home</Link>
-        <Link to="/tienda" className={styles.link}>Tienda</Link>
-        <Link to="/test" className={styles.link}>Test</Link>
-
-        <button
-          onClick={handleClick}
-          className={styles.link}
-          style={{
-            cursor: user?.rol === 'user' ? 'pointer' : 'not-allowed',
-            opacity: user?.rol === 'user' ? 1 : 0.5
-          }}
-        >
-          Solo para usuarios
-        </button>
-
-        {/* Si NO hay usuario, mostrar Login y Registro */}
-        {!user && (
-          <>
-            <LoginButton />
-            <Link to="/registro" className={styles.link}>Registrarse</Link>
-          </>
-        )}
-
-        {/* Si hay usuario, mostrar botón de logout */}
-        {user && <LogoutButton />}
-      </nav>
-    </header>
+    <nav className={styles.navbar}>
+      <Link to="/">
+        <img src="/Cartel.webp" alt="Cartel Inicio" className={`${styles.cartel} ${styles.cartelInicio}`} />
+      </Link>
+      <Link to="/tienda">
+        <img src="/Cartel.webp" alt="Cartel Tienda" className={`${styles.cartel} ${styles.cartelTienda}`} />
+      </Link>
+      <Link to="/test">
+        <img src="/Cartel.webp" alt="Cartel Test" className={`${styles.cartel} ${styles.cartelTest}`} />
+      </Link>
+      <Link to="/login">
+        <img src="/Cartel.webp" alt="Cartel Login" className={`${styles.cartel} ${styles.cartelLogin}`} />
+      </Link>
+      <Link to="/registro">
+        <img src="/Cartel.webp" alt="Cartel Registro" className={`${styles.cartel} ${styles.cartelRegistro}`} />
+      </Link>
+    </nav>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 // src/pages/Home.jsx
 // import Navbar from '../components/Navbar/Navbar'
-import { Link } from 'react-router-dom'
+
+import Navbar from '@/components/Navbar/Navbar'
 import PerroCard from '../components/PerroCard/PerroCard'
 import usePerros from '../hooks/usePerros'
 import styles from './Home.module.css'
@@ -10,27 +11,12 @@ const Home = () => {
 
   return (
     <>
-
       <header className={styles.header}>
         <img src="/Header.png" alt="Header background" className={styles.background} />
         <img src="/Gua-logo.png" alt="Gua Logo" className={styles.logo} />
 
-        {/* cinco carteles */}
-        <Link to="/" >
-          <img src="/Cartel.webp" alt="Cartel Inicio" className={`${styles.cartel} ${styles.cartelInicio}`} />
-        </Link>
-        <Link to="/tienda" >
-          <img src="/Cartel.webp" alt="Cartel Tienda" className={`${styles.cartel} ${styles.cartelTienda}`} />
-        </Link>
-        <Link to="/test" >
-          <img src="/Cartel.webp" alt="Cartel Test" className={`${styles.cartel} ${styles.cartelTest}`} />
-        </Link>
-        <Link to="/login" >
-          <img src="/Cartel.webp" alt="Cartel Login" className={`${styles.cartel} ${styles.cartelLogin}`} />
-        </Link>
-        <Link to="/registro" >
-          <img src="/Cartel.webp" alt="Cartel Registro" className={`${styles.cartel} ${styles.cartelRegistro}`} />
-        </Link>
+        {/* Navbar de carteles */}
+        <Navbar />
       </header>
       <div className={styles.wrapper}>
         {isLoading && <p>Cargando...</p>}
