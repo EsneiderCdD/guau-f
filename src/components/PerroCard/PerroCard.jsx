7b97e2031dc994522a83763fc20670508f470967
EsneiderCdD <ecd.97@hotmail.com>
2025-08-31 13:07:26 -0500

    feat(&style): personalizacion perro-card ->  perroCardStyles.js

--- src/components/PerroCard/PerroCard.jsx
+++ src/components/PerroCard/PerroCard.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import styles from './PerroCard.module.css'
+import perroCardStyles from './perroCardStyles'
 
 const PerroCard = ({ perro }) => {
   const [mostrarSegunda, setMostrarSegunda] = useState(false)
-
   const alternarImagen = () => setMostrarSegunda(prev => !prev)
+
+  // Buscar config por id (si no existe, será un objeto vacío)
+  const config = perroCardStyles[perro.id] || {}
 
   return (
     <div
@@ -19,16 +22,17 @@ const PerroCard = ({ perro }) => {
         className={`${styles.imageUno} ${!mostrarSegunda ? styles.visible : styles.oculta}`}
         src={perro.imagen_card_uno}
         alt={`${perro.nombre} imagen uno`}
+        style={config.imagenUno}   
       />
 
-      {/* Imagen 2 con estilo distinto */}
+      {/* Imagen 2 */}
       <img
         className={`${styles.imageDos} ${mostrarSegunda ? styles.visible : styles.oculta}`}
         src={perro.imagen_card_dos}
         alt={`${perro.nombre} imagen dos`}
+        style={config.imagenDos}   
       />
 
-    
       <Link className={styles.link} to={`/perros/${perro.id}`}>Ver más</Link>
     </div>
   )
---
+++ src/components/PerroCard/perroCardStyles.js
@@ -0,0 +1,34 @@
+const perroCardStyles = {
+  1: {
+    imagenUno: {
+      width: "100%",
+      top: "55%",
+      height: "auto"
+    },
+    imagenDos: {
+      top: "30%",
+    },
+  },
+  2: {
+    imagenUno: {
+      width: "auto",
+      top: "40%",
+      height: "100%"
+    },
+    imagenDos: {
+      top: "35%",
+    },
+  },
+    3: {
+    imagenUno: {
+      width: "auto",
+      top: "46%",
+      height: "75%"
+    },
+    imagenDos: {
+      top: "75%",
+    },
+  },
+}
+
+export default perroCardStyles
