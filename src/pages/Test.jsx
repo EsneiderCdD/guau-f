39a21036331c7239f487375f52e44fce7e702c01
EsneiderCdD <ecd.97@hotmail.com>
2025-09-01 12:48:12 -0500

    feat(&style): Test Pag

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -2,14 +2,21 @@
 import { useState } from 'react'
 import EncuestaForm from '@components/test/EncuestaForm'
 import Compatibilidad from '@components/test/Compatibilidad'
+import Navbar from '@components/navbar/Navbar'
+import styles from './Test.module.css'
 
 const Test = () => {
   const [showResults, setShowResults] = useState(false)
 
   return (
     <>
-      <EncuestaForm onComplete={() => setShowResults(true)} />
-      <Compatibilidad enabled={showResults} />
+      <div className={styles.container}>
+        <div className={styles.layout}>
+          <Navbar />
+        </div>
+        <EncuestaForm onComplete={() => setShowResults(true)} />
+        <Compatibilidad enabled={showResults} />
+      </div>
     </>
   )
 }
