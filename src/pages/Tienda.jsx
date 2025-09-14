3c24f4e835046b73cf2c8f186e86c4c383bdb3c4
EsneiderCdD <ecd.97@hotmail.com>
2025-09-14 15:38:16 -0500

    feat(&style) : Tienda.jsx y module css + grid template of generator

--- src/pages/Tienda.jsx
+++ src/pages/Tienda.jsx
@@ -1,4 +1,3 @@
-// src/pages/Tienda.jsx
 import Navbar from '../components/Navbar/Navbar'
 import ProductoCard from '../components/ProductoCard/ProductoCard'
 import useProductos from '../hooks/useProductos'
@@ -10,23 +9,44 @@ const Tienda = () => {
 
   return (
     <>
-  
+
       <header className={styles.header}>
         <img src="/Header2.webp" alt="Header background" className={styles.background} />
         <img src="/Gua-logo.png" alt="Gua Logo" className={styles.logo} />
       </header>
+
       <div className={styles.layout}>
         <Navbar />
       </div>
+
+      {/* COLLAGE con grid */}
+      <div className={styles.wrapper}> 
+        <div className={styles.parent}>
+          <div className={styles.div1}><img src="/tienda/tienda7.png" /></div>
+          <div className={styles.div2}><img src="/tienda/tienda2.png" /></div>
+          <div className={styles.div3}><img src="/tienda/tienda5.png" /></div>
+          <div className={styles.div4}><img src="tienda/default.png" /></div>
+          <div className={styles.div5}><img src="tienda/default.png" /></div>
+          <div className={styles.div6}><img src="/tienda/tienda1.png" /></div>
+          <div className={styles.div7}><img src="/tienda/tienda3.png" /></div>
+          <div className={styles.div8}><img src="tienda/default.png" /></div>
+          <div className={styles.div9}><img src="tienda/default.png" /></div>
+          <div className={styles.div10}><img src="tienda/default.png" /></div>
+        </div>
+      </div>
+
+      {/* Wrapper de productos (desactivado por ahora) */}
       <div className={styles.wrapper}>
+        {/* 
         {isLoading && <p>Cargando productos...</p>}
         {error && <p>Error: {error}</p>}
         {productos.map(producto => (
           <ProductoCard key={producto.id} producto={producto} />
-        ))}
+        ))} 
+        */}
       </div>
-      <AdminPanel tabs={['productos']} />
 
+      {/* <AdminPanel tabs={['productos']} /> */}
     </>
   )
 }
